330936cb8ccf657ff2db03fcd8f57d1931fabcc5
kwmStart <562752110@qq.com>
2022-02-08 21:11:02 +0800

    20220206--修改备注格式并测试提交贡献值。

--- src/main/java/com/kwm/paymentdemo/entity/RefundInfo.java
+++ src/main/java/com/kwm/paymentdemo/entity/RefundInfo.java
@@ -10,18 +10,18 @@ public class RefundInfo extends BaseEntity{
     private String orderNo;
     //退款单编号
     private String refundNo;
-
-    private String refundId;//支付系统退款单号
-
-    private Integer totalFee;//原订单金额(分)
-
-    private Integer refund;//退款金额(分)
-
-    private String reason;//退款原因
-
-    private String refundStatus;//退款单状态
-
-    private String contentReturn;//申请退款返回参数
-
-    private String contentNotify;//退款结果通知参数
+    //支付系统退款单号
+    private String refundId;
+    //原订单金额(分)
+    private Integer totalFee;
+    //退款金额(分)
+    private Integer refund;
+    //退款原因
+    private String reason;
+    //退款单状态
+    private String refundStatus;
+    //申请退款返回参数
+    private String contentReturn;
+    //退款结果通知参数
+    private String contentNotify;
 }
